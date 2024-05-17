0350cd4f22d17552d4a8836d9799b3b577d13e3f
Bill Sorensen <william.sorensen@grafana.com>
2024-05-17 16:54:23 -0600

    Add Kafka consumer

--- main.ts
+++ main.ts
@@ -1,3 +1,25 @@
-const i: string = 'bob';
+import {type EachMessagePayload, Kafka} from 'kafkajs';
 
-console.log(i);
+async function processMessage(payload: EachMessagePayload): Promise<void> {
+  console.log(payload.message.value.toString());
+};
+
+async function main() {
+  const kafka = new Kafka({
+    clientId: 'kafkajs-test',
+    brokers: ['localhost:9092'],
+  });
+  
+  const consumer = kafka.consumer({ groupId: 'my-group' });
+
+  await consumer.connect();
+  await consumer.subscribe({ topic: 'my-topic', fromBeginning: false });
+
+  await consumer.run({
+    eachMessage: processMessage,
+  });
+
+  // Note - not currently calling consumer.disconnect().
+}
+
+main().catch(console.error);
